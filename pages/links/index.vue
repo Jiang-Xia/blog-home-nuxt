8a2bb3989a520d430bde5caec9bb9992cd05dcd7
jiang-xia <963798512@qq.com>
2022-07-26 18:31:07 +0800

    fix:  1.daisyUI

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { UserAddIcon } from "@heroicons/vue/solid";
-import { TransitionRoot, Dialog, TransitionChild,ComboboxInput } from "@headlessui/vue";
 import request from "~~/api/request";
 const { data: linkList } = await useAsyncData("link_Get", () =>
   request.get("/link").then((res) => res.data)
@@ -30,83 +28,35 @@ useHead({
   <NuxtLayout name="main-content">
     <div class="links-container">
       <!-- <el-empty description="友链页面 暂时没有东西哦！" /> -->
-      <div class="h-10 flex justify-end text-xs">
-        <button
-          type="button"
-          @click="openModal"
-          class="link-btn text-white flex justify-center items-center rounded-md w-16 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"
-        >
-          <UserAddIcon class="h-6 w-6" />
-        </button>
-      </div>
+      <div class="h-10 flex justify-end text-xs"></div>
+      <!-- The button to open modal -->
+      <label for="link-add-modal" class="btn modal-button">+ 申请外链</label>
 
-      <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="closeModal" class="relative z-10">
-          <TransitionChild
-            as="template"
-            enter="duration-300 ease-out"
-            enter-from="opacity-0"
-            enter-to="opacity-100"
-            leave="duration-200 ease-in"
-            leave-from="opacity-100"
-            leave-to="opacity-0"
+      <!-- Put this part before </body> tag -->
+      <input type="checkbox" id="link-add-modal" class="modal-toggle" />
+      <div class="modal">
+        <div class="modal-box relative">
+          <label
+            for="link-add-modal"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+            >✕</label
           >
-            <div class="fixed inset-0 bg-black bg-opacity-25" />
-          </TransitionChild>
+          <h3 class="text-lg font-bold">申请外链!</h3>
 
-          <div class="fixed inset-0 overflow-y-auto">
-            <div
-              class="flex min-h-full items-center justify-center p-4 text-center"
-            >
-              <TransitionChild
-                as="template"
-                enter="duration-300 ease-out"
-                enter-from="opacity-0 scale-95"
-                enter-to="opacity-100 scale-100"
-                leave="duration-200 ease-in"
-                leave-from="opacity-100 scale-100"
-                leave-to="opacity-0 scale-95"
-              >
-                <DialogPanel
-                  class="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-4 text-left align-middle shadow-xl transition-all"
-                >
-                  <DialogTitle
-                    as="h3"
-                    class="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    申请外链
-                  </DialogTitle>
-
-                  <div class="mt-2">
-                    111
-                    <ComboboxInput
-                      class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                      @change=""
-                    />
-                  </div>
-
-                  <div class="mt-4 flex justify-end">
-                    <button
-                      type="button"
-                      class="inline-flex mr-4 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      @click="closeModal"
-                    >
-                      关闭
-                    </button>
-                    <button
-                      type="button"
-                      class="inline-flex justify-center rounded-md border border-transparent bg-blue-200 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      @click="closeModal"
-                    >
-                      确认
-                    </button>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
+          <div class="py-4">
+            <div class="form-control">
+              <label class="input-group input-group-sm">
+                <span>SM</span>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  class="input input-bordered input-sm"
+                />
+              </label>
             </div>
           </div>
-        </Dialog>
-      </TransitionRoot>
+        </div>
+      </div>
     </div>
   </NuxtLayout>
 </template>
