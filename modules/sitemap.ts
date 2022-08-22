00246868440a334147a466ba929bc91cd3346810
jiang-xia <963798512@qq.com>
2022-08-22 11:58:29 +0800

    feat:  sitemap增加详情

--- modules/sitemap.ts
+++ modules/sitemap.ts
@@ -3,7 +3,7 @@ import { Readable } from 'stream'
 import { dirname } from 'path'
 import { SitemapStream, streamToPromise } from 'sitemap'
 import { defineNuxtModule, createResolver } from '@nuxt/kit'
-
+import { $fetch } from 'ohmyfetch'
 export default defineNuxtModule({
   meta: {
     name: 'sitemap',
@@ -15,9 +15,11 @@ export default defineNuxtModule({
     hostname: 'http://localhost:5050',
   },
   async setup(options, nuxt) {
-    async function generateSitemap(routes) {
-      const sitemapRoutes = routes.map(route => route.path)
-
+    async function generateSitemap(routes,ids:[]) {
+      let sitemapRoutes = routes.map(route => route.path);
+      // console.log({sitemapRoutes,ids})
+      const details =ids.map((v:any)=>'detail/'+v);
+      sitemapRoutes = sitemapRoutes.concat(details)
       // https://github.com/ekalinin/sitemap.js#generate-a-one-time-sitemap-from-a-list-of-urls
       const stream = new SitemapStream({ hostname: options.hostname })
       return streamToPromise(Readable.from(sitemapRoutes).pipe(stream)).then(data =>
@@ -44,7 +46,15 @@ export default defineNuxtModule({
     })
 
     nuxt.hook('pages:extend', async pages => {
-      const sitemap = await generateSitemap(pages)
+      const res:any = await $fetch("https://jiang-xia.top/x-api/blog-server/article/list",{
+        method:'post',
+        body:{
+          page:1,
+          pageSize:500
+        }
+      })
+      const articleIds = res.data.list.map((v:any)=>v.id)
+      const sitemap = await generateSitemap(pages,articleIds)
       createSitemapFile(sitemap, filePath)
     })
   },
