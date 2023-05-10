f9db238edef1c36753eb2559453fc4e8c8e0f201
jiang-xia <963798512@qq.com>
2023-05-10 15:40:47 +0800

    fix:  1.首页样式优化

--- api/index.ts
+++ api/index.ts
@@ -22,13 +22,18 @@ export const gushici = async () => {
   })
   return res.data
 }
-
+// 获取天气
+export const getWeather = async () => {
+  const res = await request.get('/resources/weather')
+  return res.data
+}
 const api = {
   ...article,
   ...tag,
   ...category,
   getUserInfo,
   gushici,
+  getWeather,
 }
 // https://gitee.com/api/v5/repos/jiang-xia/blog-home-nuxt/commits?access_token=53db1802db341bf994093cace04c275b&page=1&per_page=20
 export default api
