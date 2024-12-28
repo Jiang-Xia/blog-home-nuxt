14f9b5e6fccf50e07691f63f3503dbdfd72ed9d7
JiangXia <963798512@qq.com>
2024-12-29 00:50:20 +0800

    fix: crypto.js

--- utils/crypto.ts
+++ utils/crypto.ts
@@ -1,6 +1,5 @@
 // 'use-client'
-/* eslint-disable */
-import CryptoJS, { enc, mode, AES, pad } from 'crypto-js'
+import CryptoJS from 'crypto-js'
 // 加密密钥（长度必须是 16 的整数倍，此处为 32 位）
 const secretKey = '54050000778e380000fe5a120000b4ce'
 // 偏移量
@@ -13,18 +12,18 @@ const iv = 'jiangxia'
  * @param {string} offset - 偏移量
  * @return 16进制字符串 256位
  */
-export function aesEncrypt(word: any, key = secretKey, offset = iv) {
+export function aesEncrypt (word: any, key = secretKey, offset = iv) {
   // 未加密的参数 - 从 UTF-8编码 解析出原始字符串
-  const wordUTF8 = enc.Utf8.parse(word)
+  const wordUTF8 = CryptoJS.enc.Utf8.parse(word)
   // 密钥 - 从 UTF-8编码 解析出原始字符串
-  const keyUTF8 = enc.Utf8.parse(key)
+  const keyUTF8 = CryptoJS.enc.Utf8.parse(key)
   // 偏移量 从 UTF-8编码 解析出原始字符串
-  const offsetUTF8 = enc.Utf8.parse(offset)
+  const offsetUTF8 = CryptoJS.enc.Utf8.parse(offset)
 
-  const encrypted = AES.encrypt(wordUTF8, keyUTF8, {
+  const encrypted = CryptoJS.AES.encrypt(wordUTF8, keyUTF8, {
     iv: offsetUTF8,
-    mode: mode.CBC,
-    padding: pad.Pkcs7,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
   })
   // 转成16进制 变成大写不影响解密
   return encrypted.toString(CryptoJS.format.Hex).toUpperCase()
@@ -38,26 +37,26 @@ export function aesEncrypt(word: any, key = secretKey, offset = iv) {
  * @param {string} offset - 偏移量
  * @return utf8 字符串
  */
-export function aesDecrypt(encryptedWord: any, key = secretKey, offset = iv) {
+export function aesDecrypt (encryptedWord: any, key = secretKey, offset = iv) {
   // 密钥 - 从 UTF-8编码 解析出原始字符串
-  const keyUTF8 = enc.Utf8.parse(key)
+  const keyUTF8 = CryptoJS.enc.Utf8.parse(key)
   // 偏移量 从 UTF-8编码 解析出原始字符串
-  const offsetUTF8 = enc.Utf8.parse(offset)
+  const offsetUTF8 = CryptoJS.enc.Utf8.parse(offset)
   // 解析十六进制字符串
   encryptedWord = CryptoJS.format.Hex.parse(encryptedWord)
   // console.log('encryptedWord:',encryptedWord)
-  const bytes = AES.decrypt(encryptedWord, keyUTF8, {
+  const bytes = CryptoJS.AES.decrypt(encryptedWord, keyUTF8, {
     iv: offsetUTF8,
-    mode: mode.CBC,
-    padding: pad.Pkcs7,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
   })
 
-  return bytes.toString(enc.Utf8)
+  return bytes.toString(CryptoJS.enc.Utf8)
 }
 
 // const encrypted2 =  aesEncrypt('========Message=======')
 // const decrypted2 =  aesDecrypt(encrypted2)
-// console.log('AES:',{encrypted2,decrypted2});
+// console.log('CryptoJS.AES:',{encrypted2,decrypted2});
 
 export default {
   aesEncrypt,
